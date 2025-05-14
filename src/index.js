438c297c1cbf9de72d2abcfe2e58ca2f9cab45c1
iyinolawumi <olatreasure90@gmail.com>
2025-05-14 10:38:03 +0100

    Added the alert and edited the structured strings.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 alert("Hello, Welcome to the AI Poem generator")
+function generatePoem(event) {
+    event.preventDefault();
+  
+    new Typewriter("#poem", {
+      strings: "Love; A whisper soft, yet strong and true, \n A spark that paints the darkest hue. \n It blooms in hearts without a sound, \n Where silent joys and tears are found.",
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+  }
+  
+  let poemFormElement = document.querySelector("#poem-generator-form");
+  poemFormElement.addEventListener("submit", generatePoem);
