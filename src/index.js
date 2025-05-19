0c7fbcfa4c08b541d984ccd87f1cb475415ecaa5
iyinolawumi <olatreasure90@gmail.com>
2025-05-19 12:08:03 +0100

    Integrated the use of API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 alert("Hello, Welcome to the AI Poem generator")
+
+function displayPoem(respons) {
+  new Typewriter("#poem", {
+    Strings: response.data.answer,
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
+}
+
 function generatePoem(event) {
     event.preventDefault();
+
+    let apiKey = "027a3305d8coee741ce3fa3097t6b4ea";
+    let prompt = "";
+    let context = "";
+    let apiURL = 'https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}';
   
-    new Typewriter("#poem", {
-      strings: "Love; A whisper soft, yet strong and true, \n A spark that paints the darkest hue. \n It blooms in hearts without a sound, \n Where silent joys and tears are found.",
-      autoStart: true,
-      delay: 1,
-      cursor: "",
-    });
+    console.log("generation poem");
+    axios.get(apiURL).then(displayPoem);
+    
   }
   
   let poemFormElement = document.querySelector("#poem-generator-form");
