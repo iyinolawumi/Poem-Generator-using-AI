bdc171668ab118db7de314a157b11aa800c07849
iyinolawumi <olatreasure90@gmail.com>
2025-05-20 12:41:42 +0100

    Added a hidden element to the js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
 
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "027a3305d8coee741ce3fa3097t6b4ea";
-  let context = "You are an AI assistant who loves to write short poems. Your mission is to write a 4-line poem in basic html and separate each line with <br />. Remove ```html in the text. Make sure to follow the user instructions below.";
+  let context = "You are an AI assistant who loves to write short poems. Your mission is to write a 4-line poem in basic html and separate each line with <br />. Make sure to follow the user instructions below.";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
  
@@ -23,7 +23,11 @@ function generatePoem(event) {
   console.log(`Prompt: ${prompt}`);
   console.log(`Context: ${context}`);
 
-  axios.get(apiURL)
+let poemElement = document.querySelector("#poem");
+poemElement.classList.remove("hidden");
+poemElement.innerHTML = `<div class="generating"> Generating a poem about ${instructionsInput.value}</div>`;
+
+ axios.get(apiURL)
     .then(displayPoem);
 }
 
